2d3fcdbace967a16293b516bcd22c19b7e0a39ee
Thomas Brian Holdren <tbholdren@gmail.com>
2019-11-18 20:54:57 -0500

    Adding mysql startup.

--- web-setup2.sh
+++ web-setup2.sh
@@ -4,4 +4,15 @@ pwd
 
 cd docker
 
+# clone credentials repository
+if [ ! -d "holdren-net-credentials" ]; then
+    git clone https://github.com/tbholdren/holdren-net-credentials.git
+fi
+
+source holdren-net-credentials/credentials.sh
+
+# setup docker networking
 bash holdren-net-setup.sh
+
+# install mysql
+bash mysql-run.sh
