7a1f30a0b9ace7751bfcc82c563f70d49d6a2b30
Thomas Brian Holdren <tbholdren@gmail.com>
2019-11-18 20:56:16 -0500

    Adding mysql startup.

--- web-setup2.sh
+++ web-setup2.sh
@@ -6,6 +6,7 @@ cd docker
 
 # clone credentials repository
 if [ ! -d "holdren-net-credentials" ]; then
+    echo cloning credentials repository
     git clone https://github.com/tbholdren/holdren-net-credentials.git
 fi
 
